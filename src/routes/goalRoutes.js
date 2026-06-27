be110e1ac98f2373edd12106c6664a5b5c4f7f28
Yair2332 <yairlezcano4@gmail.com>
2026-06-27 01:39:50 -0300

    feat: update goalControllers withdrawContribution

--- src/routes/goalRoutes.js
+++ src/routes/goalRoutes.js
@@ -4,6 +4,7 @@ const goalController = require('../controllers/goalController');
 
 router.post('/', goalController.createGoal); 
 router.get('/:userId', goalController.getGoals);
+router.post('/withdraw', goalController.withdrawContribution);
 router.post('/contribution', goalController.addContribution);
 router.patch('/:goalId', goalController.updateGoal);
 router.delete('/:goalId', goalController.deleteGoal);
